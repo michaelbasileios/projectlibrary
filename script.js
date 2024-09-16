2bec7f23ac5c885b3ca78ecbe6767960542695e3
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2024-09-16 15:22:13 -0500

    Create rudimentary function to iterate over library

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: "Hobbit",
+    author: "Tolkien",
+    pages: 326,
+    readStatus: true
+    },
+    {title: "Dracula",
+     author: "Stroker",
+     pages: 427,
+     readStatus: false
+    }
+];
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -9,4 +20,10 @@ function Book(title, author, pages, readStatus) {
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
+}
+
+function iterateArray(array) {
+    for (i=0; i<array.length; i++) {
+        console.log(array[i]);
+    }
 }
