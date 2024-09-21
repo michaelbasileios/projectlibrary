a7de6e945d5d535431e383b5f98e14a9e431a2ea
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2024-09-21 11:13:21 -0500

    Create function for displaying books in grid

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ const myLibrary = [
     }
 ];
 
+//constructor function for books
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+}
+
+//function that iterates through array and displays books
+function addBook() {
+    libraryGrid.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        displayBook(myLibrary[i]);
+    }
 }
